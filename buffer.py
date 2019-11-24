cff02be0bf74280278741efe5a8aabc6c574a694
Denis Luciano <denis.lopes3012@gmail.com>
2019-11-24 12:20:34 -0800

    mais ou menos feito

--- buffer.py
+++ buffer.py
@@ -1,6 +1,10 @@
 import socket
+import random
+import select
 
-contador = 0
+contador = 1
+inn = 1
+out = 1
 lugares = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
@@ -10,13 +14,54 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
-while True:
+
+
+# Aguardando as duas conexoes do produtor e 
+# consumidor serem estabelecidas
+for _ in range(0,2):
     con, cliente = tcp.accept()
-    print ('Concetado por', cliente)
+    if con.recv(4).decode() == 'prod':
+        print ('Concetado pelo produtor: ', cliente)
+        prod=con
+    else:
+        print ('Concetado pelo consumidor: ', cliente)
+        cons=con
+    
+  
+inputs=[prod,cons]
+
+
+
+while True:
+
     while contador < 100:
-        contador+=1
-        msg = con.recv(1024)
-        if not msg: break
-        print(cliente, contador)
+        # Parcialmente cheio
+        if contador > 0 and contador < 1000:
+            print("entrou aqui dentro")
+            entrada,_,erro=select.select(inputs, [], inputs)
+            con = entrada[random.randint(0,len(entrada)-1)]
+            msg = con.recv(1024)
+            print("passou select e con : ")
+            print(con)
+
+            if con is prod:
+                print("produtor em açao", end = '')
+                if not msg: break
+                print("mensagem produtor: ", msg, end = '')
+
+            if con is cons:
+                print("consumidor em açao", end = '')
+                if not msg: break
+                print("mensagem consumidor: ", msg, end = '')
+        # Cheio ou vazio
+        else:
+            if contador == 0: #buffer vazio
+                print("ta vazio")
+            else: #buffer cheio
+                print("ta cheio")
+
+        contador += 1
+
+
     print ('Finalizando conexao do cliente', cliente)
     con.close()
--- produtor.py
+++ produtor.py
@@ -6,7 +6,13 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
 print ('Para sair use CTRL+X\n')
-msg = "dewdwe"
+
+tipo = "prod"
+conteudo = "batata"
+
+msg = tipo+conteudo
+
+
 while True:
     tcp.send (msg.encode())
 tcp.close()
